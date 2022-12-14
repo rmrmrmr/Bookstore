bbb97f79c53c40973db876ee6f95686c4b521d8c
rmrmrmr <ricardomartineza77@gmail.com>
2022-12-14 15:31:31 -0600

    Add Redux functionality to Categories page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
-import AddBook from './components/AddBook';
-import Navbar from './components/Navbar';
-import List from './components/List';
+import Categories from './pages/Categories';
+import Book from './pages/Books';
 
 export default function App() {
   return (
@@ -15,20 +14,13 @@ export default function App() {
         <Route
           path="/"
           element={(
-            <div className="h-screen">
-              <Navbar />
-              <List />
-              <AddBook />
-            </div>
+            <Book />
 )}
         />
         <Route
           path="/categories"
           element={(
-            <div className="h-screen flex flex-wrap justify-center">
-              <Navbar />
-              <input type="button" value="Check Status" className="bg-blue-500 text-white px-10 py-2 text-sm w-1/6 h-9" />
-            </div>
+            <Categories />
 )}
         />
       </Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { RiUser5Line } from 'react-icons/ri';
 
-export default function Books() {
+export default function Navbar() {
   return (
     <header className="flex gap-20 items-center w-screen h-16 px-20 border-slate-300 bg-white border-b-2">
       <h1 className="text-blue-500 text-3xl font-bold">Bookstore CMS</h1>
